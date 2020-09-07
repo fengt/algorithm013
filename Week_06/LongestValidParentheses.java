e74ffdd6d77841e3135f1ce6cbe774f389f18143
Marco Feng <fhalcyon@gmail.com>
2020-09-07 15:08:32 +0800

    32. Added approach 3: Two Counters

--- Week_06/LongestValidParentheses.java
+++ Week_06/LongestValidParentheses.java
@@ -1,22 +1,14 @@
 package org.fengt.leetcode.frequency.num_32;
 
-import org.junit.Test;
-
 import java.util.ArrayDeque;
 import java.util.Deque;
 
 public class LongestValidParentheses {
 
-    @Test
-    public void test() {
-        String s = ")(()())";
-        System.out.println(longestValidParentheses(s));
-    }
-
     /**
      * Approach 1: Stack
      * Time complexity: O(N)
-     * Space complexity: O(1)
+     * Space complexity: O(N)
      * @param s
      * @return
      */
@@ -41,10 +33,28 @@ public class LongestValidParentheses {
         return res;
     }
 
+    public int longestValidParentheses11(String s) {
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') stack.push(i);
+            else if (!stack.empty() && s.charAt(stack.peek()) == '(') stack.pop();
+            else stack.push(i);
+        }
+        if (stack.empty()) return s.length();
+        int res = 0, high = s.length();
+
+        while (!stack.empty()) {
+            int low = stack.pop();
+            res = Math.max(res, high - low - 1);
+            high = low;
+        }
+        return Math.max(res, high);
+    }
+
     /**
      * Approach 2: DP
      * Time complexity: O(N)
-     * Space complexity: O(1)
+     * Space complexity: O(N)
      * @param s
      * @return
      */
@@ -61,6 +71,45 @@ public class LongestValidParentheses {
                 }
             }
             res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+
+    /**
+     * Approach 3: Two Counter
+     * Time complexity: O(N)
+     * Space complexity: O(1)
+     * @param s
+     * @return
+     */
+    public int longestValidParentheses3(String s) {
+        int left = 0, right = 0, res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                left++;
+            } else {
+                right++;
+            }
+
+            if (left == right) {
+                res = Math.max(res, 2 * left);
+            } else if (right > left) {
+                left = right = 0;
+            }
+        }
+        left = right = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == '(') {
+                left++;
+            } else {
+                right++;
+            }
+
+            if (left == right) {
+                res = Math.max(res, 2 * left);
+            } else if (left > right) {
+                left = right = 0;
+            }
         }
         return res;
     }
